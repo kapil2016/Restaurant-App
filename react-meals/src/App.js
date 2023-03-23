19fe9235d46b33d25cfb1202afda76b979dcf91d
kapil kumar <kapilkumardu.ramjas@gmail.com>
2023-03-23 07:16:44 +0530

    meals summery added

--- react-meals/src/App.js
+++ react-meals/src/App.js
@@ -1,8 +1,12 @@
 import Header from "./Layout/Header/Header";
+import MealsSummary from "./Meals/MealsSummery";
 function App() {
   return (
     <div>
       <Header></Header>
+      <main>
+        <MealsSummary></MealsSummary>
+      </main>
     </div>
   );
 }
